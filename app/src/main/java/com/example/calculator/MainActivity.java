5b386f66ad9fa06a6cc0c25f6aa28c62b8beb5b2
Aleksey Iskovskikh <iskovskikh56@gmail.com>
2020-01-19 17:21:31 +0500

    Доработки

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -27,93 +27,94 @@ public class MainActivity extends AppCompatActivity {
         Button btn7=findViewById(R.id.num7);
         Button btn8=findViewById(R.id.num8);
         Button btn9=findViewById(R.id.num9);
-        Button btntochka=findViewById(R.id.num);
+        Button btntochka=findViewById(R.id.numtochka);
+
         btn0.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num0);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn1.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num1);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num2);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num3);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn4.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num4);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn5.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num5);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn6.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num6);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn7.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num7);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn8.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num8);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btn9.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_num9);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
         btntochka.setOnClickListener(new View.OnClickListener() {
-            TextView txt = findViewById(R.id.number);
+            TextView disp = findViewById(R.id.disp);
             @Override
             public void onClick(View v) {
                 String text = getString(R.string.txt_numtochka);
-                txt.setText(text);
+                disp.setText(text);
             }
         });
     }
